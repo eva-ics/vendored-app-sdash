eb5e5f8b1ec8e7eda4b958506f50c1e45cb57e6c
Serhij S <div@altertech.com>
2023-11-06 23:37:38 +0100

    fmt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
       <button className="menu-icon-btn" onClick={toggleMenu}>
         <GiHamburgerMenu size={25} />
       </button>
-      <div className="header-info">Node: {eva.system_name()}</div>
+      <div className="header-info">EVA ICS SDash. Node: {eva.system_name()}</div>
       <nav id="header">
         <ul>
           {nav.map((v, idx) => {
