4a421df543db11db712b176c94ec39488da4b244
RMerkulov <99145947+RoRomario360@users.noreply.github.com>
2023-12-12 13:35:34 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,38 +5,42 @@ import { get_engine } from "@eva-ics/webengine-react";
 import { NavLink } from "react-router-dom";
 
 const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
-  const eva = get_engine() as Eva;
-  return (
-    <header className="header">
-      <button className="menu-icon-btn" onClick={toggleMenu}>
-        <GiHamburgerMenu size={25} />
-      </button>
-      <div className="header-info">
-        <div className="dash-info">
-          <img src="icon.svg" className="dash-logo" />
-          <div className="dash-title">
-            EVA ICS SDash. Node: {eva.system_name()}
-          </div>
-        </div>
-      </div>
-      <nav id="header">
-        <ul>
-          {nav.map((v, idx) => {
-            let class_name = "nav-link";
-            let class_name_container = "nav-link-container";
-             let isCurrent = false;
-            if (current_page == v.value) {
-              class_name += " nav-link-current";
-              class_name_container += " nav-link-container-current";
-               isCurrent = true;
-            }
-            return (
-              <li className={class_name} key={idx}>
-                <NavLink key={idx} to={v.to}>
-                  <div className={class_name_container}>{v.value}</div>
-                </NavLink>
+    const eva = get_engine() as Eva;
 
-                 {isCurrent && v.submenus && v.submenus.length > 0 && (
+    return (
+        <header className="header">
+            <button className="menu-icon-btn" onClick={toggleMenu}>
+                <GiHamburgerMenu size={25} />
+            </button>
+            <div className="header-info">
+                <div className="dash-info">
+                    <img src="icon.svg" className="dash-logo" />
+                    <div className="dash-title">
+                        EVA ICS OpCentre. Node: {eva.system_name()} [
+                        {eva?.server_info?.aci.u}]
+                    </div>
+                </div>
+            </div>
+            <nav id="header">
+                <ul>
+                    {nav.map((v, idx) => {
+                        const isCurrent = current_page === v.value;
+
+                        const navLinkClass = isCurrent
+                            ? "nav-link nav-link-current"
+                            : "nav-link";
+
+                        const containerClass = isCurrent
+                            ? "nav-link-container nav-link-container-current"
+                            : "nav-link-container";
+
+                        return (
+                            <li className={navLinkClass} key={idx}>
+                                <NavLink key={idx} to={v.to}>
+                                    <div className={containerClass}>{v.value}</div>
+                                </NavLink>
+
+                                {isCurrent && v.submenus && v.submenus.length > 0 && (
                                     <ul className="submenu">
                                         {v.submenus.map((submenuItem, subIdx) => (
                                             <li className="submenu-item" key={subIdx}>
@@ -53,14 +57,13 @@ const Header = ({ toggleMenu, nav, logout, current_page }: HeaderProps) => {
                                         ))}
                                     </ul>
                                 )}
-              </li>
-            );
-          })}
-          
-        </ul>
-      </nav>
-    </header>
-  );
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
