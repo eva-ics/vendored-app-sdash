3c8babc1bf8ba5b22658cf07990a538da67b79f5
Serhij S <div@altertech.com>
2023-12-13 00:47:59 +0100

    va links

--- src/sdash.tsx
+++ src/sdash.tsx
@@ -6,37 +6,38 @@ import { DEFAULT_TITLE } from "./types/index.tsx";
 import { Eva } from "@eva-ics/webengine";
 
 const SDash = ({ logout }: { logout: FunctionLogout }) => {
-  const eva = get_engine() as Eva;
+    const eva = get_engine() as Eva;
 
-  useEffect(() => {
-    const system_name = eva.system_name();
-    document.title = `${system_name} system dashboard`;
-    return () => {
-      document.title = DEFAULT_TITLE;
-    };
-  }, []);
+    useEffect(() => {
+        const system_name = eva.system_name();
+        document.title = `${system_name} system dashboard`;
+        return () => {
+            document.title = DEFAULT_TITLE;
+        };
+    }, []);
 
-  if (eva.server_info.acl.admin) {
-    return (
-      <BrowserRouter>
-        <Layout logout={logout}></Layout>
-      </BrowserRouter>
-    );
-  } else {
-    return (
-      <div className="admin-required-container">
-        <div className="admin-required">Admin access required</div>
-        <div className="admin-required-info">
-          To use this application, please logout and re-login as an admin user
-        </div>
-        <div className="admin-required-logout">
-          <a href="#" onClick={logout}>
-            Logout
-          </a>
-        </div>
-      </div>
-    );
-  }
+    if (eva.server_info.acl.admin) {
+        return (
+            <BrowserRouter>
+                <Layout logout={logout}></Layout>
+            </BrowserRouter>
+        );
+    } else {
+        return (
+            <div className="admin-required-container">
+                <div className="admin-required">Admin access required</div>
+                <div className="admin-required-info">
+                    To use this application, please logout and re-login as an admin user
+                </div>
+                <div className="admin-required-links">
+                    <a href="/va/">Vendored apps</a>
+                    <a href="#" onClick={logout}>
+                        Logout
+                    </a>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default SDash;
