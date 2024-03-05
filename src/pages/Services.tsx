8234cb3a9b7b1006e31cc2dcdcd0f3d250b8fa8a
Serhij S <div@altertech.com>
2024-03-05 03:21:01 +0100

    hide restart btn in report

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -6,100 +6,103 @@ import { DashTable, DashTableData, DashTableFilter } from "bmat/dashtable";
 import { useQueryParams } from "bmat/hooks";
 
 const DashboardServices = () => {
-  const eva = useMemo(() => {
-    return get_engine() as Eva;
-  }, []);
+    const eva = useMemo(() => {
+        return get_engine() as Eva;
+    }, []);
 
-  const [params, setParams] = useState({
-    filter: null
-  });
-
-  const setSvcParams = (p: object) => {
-    let np: any = { ...params };
-    Object.keys(p).forEach((k) => {
-      np[k] = (p as any)[k];
+    const [params, setParams] = useState({
+        filter: null,
     });
-    setParams(np);
-  };
 
-  const loaded = useQueryParams(
-    [
-      {
-        name: "filter",
-        value: params.filter || "",
-        setter: (s: string) => {
-          setSvcParams({ filter: s || null });
-        }
-      }
-    ],
-    [params.filter]
-  );
-
-  const svc_list = useEvaAPICall({
-    method: loaded ? `bus::eva.core::svc.list` : undefined,
-    params: params,
-    update: 1
-  });
-
-  const restartService = (id: string) => {
-    eva
-      .call(`bus::eva.core::svc.restart`, { i: id })
-      .then(() => onSuccess())
-      .catch((e) => onEvaError(e));
-  };
-
-  const filter: DashTableFilter = [
-    [
-      "Filter",
-      <input
-        size={20}
-        value={params.filter || ""}
-        onChange={(e) => setSvcParams({ filter: e.target.value || null })}
-      />
-    ]
-  ];
-
-  const data: DashTableData = svc_list?.data?.map((svc: any) => {
-    return {
-      data: [
-        { value: svc.id },
-        {
-          value: svc.status,
-          className:
-            "col-fit " +
-            (svc.status == "online" ? "data-active" : "data-inactive")
-        },
-        { value: svc.pid, className: "col-fit" },
-        {
-          value: svc.pid ? (
-            <button onClick={() => restartService(svc.id)}>restart</button>
-          ) : null,
-          className: "col-fit"
-        },
-        { value: svc.launcher }
-      ]
+    const setSvcParams = (p: object) => {
+        let np: any = { ...params };
+        Object.keys(p).forEach((k) => {
+            np[k] = (p as any)[k];
+        });
+        setParams(np);
     };
-  });
 
-  return (
-    <div>
-      <div className="dashboard-main-wrapper dashboard-main-wrapper-big">
-        <div className="dashboard-main-wrapper-content">
-          <div className="dashboard-main-wrapper-content__side-left">
-            <DashTable
-              id="svcs"
-              title="Services"
-              filter={filter}
-              cols={["id", "status", "pid", "", "launcher"]}
-              data={data}
-              className="content-longtable"
-              rememberQs={true}
-            />
-          </div>
+    const loaded = useQueryParams(
+        [
+            {
+                name: "filter",
+                value: params.filter || "",
+                setter: (s: string) => {
+                    setSvcParams({ filter: s || null });
+                },
+            },
+        ],
+        [params.filter]
+    );
+
+    const svc_list = useEvaAPICall({
+        method: loaded ? `bus::eva.core::svc.list` : undefined,
+        params: params,
+        update: 1,
+    });
+
+    const restartService = (id: string) => {
+        eva.call(`bus::eva.core::svc.restart`, { i: id })
+            .then(() => onSuccess())
+            .catch((e) => onEvaError(e));
+    };
+
+    const filter: DashTableFilter = [
+        [
+            "Filter",
+            <input
+                size={20}
+                value={params.filter || ""}
+                onChange={(e) => setSvcParams({ filter: e.target.value || null })}
+            />,
+        ],
+    ];
+
+    const data: DashTableData = svc_list?.data?.map((svc: any) => {
+        return {
+            data: [
+                { value: svc.id },
+                {
+                    value: svc.status,
+                    className:
+                        "col-fit " +
+                        (svc.status == "online" ? "data-active" : "data-inactive"),
+                },
+                { value: svc.pid, className: "col-fit" },
+                {
+                    value: svc.pid ? (
+                        <div className="print-hidden">
+                            <button onClick={() => restartService(svc.id)}>
+                                restart
+                            </button>
+                        </div>
+                    ) : null,
+                    className: "col-fit",
+                },
+                { value: svc.launcher },
+            ],
+        };
+    });
+
+    return (
+        <div>
+            <div className="dashboard-main-wrapper dashboard-main-wrapper-big">
+                <div className="dashboard-main-wrapper-content">
+                    <div className="dashboard-main-wrapper-content__side-left">
+                        <DashTable
+                            id="svcs"
+                            title="Services"
+                            filter={filter}
+                            cols={["id", "status", "pid", "", "launcher"]}
+                            data={data}
+                            className="content-longtable"
+                            rememberQs={true}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DashboardServices;
