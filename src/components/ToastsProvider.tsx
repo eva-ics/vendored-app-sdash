f165656d4af0c759e9d05f55d01f09bbd2f49a30
Serhij S <s@g5.is>
2024-11-29 03:32:47 +0100

    toast bottom left

--- src/components/ToastsProvider.tsx
+++ src/components/ToastsProvider.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "react-hot-toast";
 
 const ToasterProvider = () => {
-  return <Toaster position={"bottom-right"} />;
+  return <Toaster position={"bottom-left"} />;
 };
 
 export default ToasterProvider;
