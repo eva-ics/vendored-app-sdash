1489994e2633ed940944c0388b08c7b00c62bd88
Tasha Natasha <0955712824n@gmail.com>
2024-12-23 15:04:39 +0200

    fix warning api sass

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  plugins: [react()],
-  css: {
-    devSourcemap: true,
-  },
+    base: "",
+    plugins: [react()],
+    css: {
+        devSourcemap: true,
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ["legacy-js-api"],
+            },
+        },
+    },
 });
